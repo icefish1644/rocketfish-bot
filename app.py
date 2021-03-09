438308447becbc7b5c057e8df4c3edb1296404cc
Gilbert Teh <jteh008@e.ntu.edu.sg>
2021-03-10 03:18:46 +0800

    2nd try

--- app.py
+++ app.py
@@ -30,14 +30,14 @@ def respond():
     if update.callback_query is not None:
         chat_id = update.callback_query.message.chat.id
         boolIsCallbackQuery = True
-    elif update.message.chat.type == 'group' and update.message.text is None:
-        chat_id = update.message.chat_id
-        boolIsNewGroup = True
     elif update.edited_message.chat.type == 'group':
         chat_id = update.edited_message.chat.id
         msg_id = update.edited_message.message_id
         text = update.edited_message.text.encode('utf-8').decode()
         boolIsEditedMessage = True
+    elif update.message.chat.type == 'group' and update.message.text is None:
+        chat_id = update.message.chat_id
+        boolIsNewGroup = True
     elif update.message is None:
         chat_id = update.edited_message.chat.id
         msg_id = update.edited_message.message_id
